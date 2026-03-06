c8f4ba3830ae031a96165f208f341ff3846c6b32
demoki787-Qa <demoki787@gmail.com>
2026-03-05 23:47:16 -0300

    Update validacoes.sql

--- Queries/validacoes.sql
+++ Queries/validacoes.sql
@@ -1,1 +1,17 @@
+-- Queries utilizadas para validação de integridade dos dados durante os testes
 
+-- Verificar usuários duplicados
+SELECT email, COUNT(*)
+FROM usuarios
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+-- Verificar pedidos sem usuário associado
+SELECT *
+FROM pedidos
+WHERE usuario_id IS NULL;
+
+-- Verificar produtos com preço inválido
+SELECT *
+FROM produtos
+WHERE preco <= 0;
